f28ce0898de0a822aee88366f1b6013560f91a7b
Michal Gorgolewski <gorgolewskimichal@gmail.com>
2020-08-31 22:08:44 +0200

    v 0.2

--- main.js
+++ main.js
@@ -1,37 +1,7 @@
 let control = {};
 
-// const module = (() => {
-//   const add = (a, b) => a + b;
-
-//   return {
-//     add,
-//   };
-// })();
-
-// const factory = (name, level) => {
-//     let health = level * 2;
-//     const getLevel = () => level;
-//     const getName  = () => name;
-//     const die = () => {
-//       // uh oh
-//     };
-//     const damage = x => {
-//       health -= x;
-//       if (health <= 0) {
-//         die();
-//       }
-//     };
-//     const attack = enemy => {
-//       if (level < enemy.getLevel()) {
-//         damage(1);
-//         console.log(`${enemy.getName()} has damaged ${name}`);
-//       }
-//     };
-//     return {attack, damage, getLevel, getName}
-//   };
-
 const Gameboard = (() => {
-  let arr = ["", "0", "x", "0", "x", "0", "x", "0", "x"];
+  let arr = ["", "", "", "", "", "", "", "", ""];
   return {
     arr,
   };
@@ -42,20 +12,24 @@ for (let i = 0; i < 9; i++) {
   const div = document.createElement("DIV");
   div.classList.add("gameboard-div");
   div.innerHTML = Gameboard.arr[i];
+  div.dataset.place = i;
   wrapper.appendChild(div);
 }
-const players = () => {
-  let turn = 0;
-  const playerOneMove = function () {
+
+const players = (name) => {
+  const playerOneMove = () => {
     const divs = document.querySelectorAll(".gameboard-div");
     divs.forEach((div) => {
       div.addEventListener("click", () => {
-        Gameboard.arr[0] = "x";
-        // dodać innerHTML
-        // console.log("xxx");
+        z = div.dataset.place;
+        Gameboard.arr[z] = "x";
+        div.innerText = "x";
       });
     });
   };
-  return { playerOneMove };
+  return { name, playerOneMove };
 };
-players.playerOneMove;
+
+const player = players("player");
+
+player.playerOneMove();
